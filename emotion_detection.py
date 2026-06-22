d0af72f87c8ad48b2f626b50218271f497836118
RahulDandavolu <rahul.dandavolu@gmail.com>
2026-06-22 15:21:42 -0400

    Bug fix 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 def emotion_detector(text_to_analyze):
     website = ("https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict")
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
